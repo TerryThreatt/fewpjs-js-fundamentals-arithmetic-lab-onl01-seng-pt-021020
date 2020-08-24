77181ca428563dff4fc4d9e0155fefd449c7731c
TerryThreatt <terry.threatt@gmail.com>
2020-08-24 13:13:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+// numbers
+const num1 = 31;
+const num2 = 2;
+
+// arithmetic
+const multiply = num1 * num2;
+
+const random = Math.random();
+
+const mod = 16 % 6;
+
+const max = Math.max( 10, 15, 20);
